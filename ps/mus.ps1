2e7443528a673ac8801d8bc5ca0e134ca1fe2f1c
wither7007 <jimsteffes@outlook.com>
2022-03-16 10:31:57 -0500

    ok

--- ps/mus.ps1
+++ ps/mus.ps1
@@ -1,3 +1,3 @@
 #runs vlc with directory
-cd \you\class
-vlc '.\Mozart - Symphony No. 41 in C, K. 551 [complete] (Jupiter).mp3' '.\Beethoven - Symphony No. 3 in E flat major (Op. 55) Eroica Berliner Philharmoniker.mp3'
+cd \music\mozart
+vlc .
