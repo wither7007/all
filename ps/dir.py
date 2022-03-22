85ab1e032246893318775d3fe69610b78c7855ec
wither7007 <jimsteffes@outlook.com>
2022-03-22 00:31:22 -0500

    ok

--- ps/dir.py
+++ ps/dir.py
@@ -1,4 +1,27 @@
 import os
-folder="."
-for count, filename in enumerate(os.listdir(folder)):
-  print(filename)
+import re
+import clipboard
+clipboard.copy("abc")  # now the clipboard content will be string "abc"
+
+folder = r"c:\all"
+myFiles = os.listdir(folder)
+# for count, filename in enumerate(os.listdir(folder)):
+#     print(count, filename)
+
+
+def filter_fun(list1):
+    # Search data based on regular expression in the list
+    return [val for val in list1
+            if re.search(r'jpg', val)]
+
+
+# print(filter_fun(myFiles))
+
+ff = filter_fun(myFiles)
+f1 = ''
+for i in ff:
+    f1 += '<img src="' + i+'">\n'
+print(f1)
+
+clipboard.copy(f1)
+# <img src="" alt="">
