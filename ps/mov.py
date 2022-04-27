f7e9e0785108032a3ef4f2f0f1647e14fcfc5d42
wither7007 <jimsteffes@outlook.com>
2022-04-27 16:40:45 -0500

    ok

--- ps/mov.py
+++ ps/mov.py
@@ -8,4 +8,9 @@ print([(n, f) for n, f in enumerate(files)])
 filenumber = int(input("File number: "))
 start = int(input("Start: "))
 end = int(input("End: "))
-ffmpeg_extract_subclip(files[filenumber], start, end, targetname="output_selected.mp4")
+
+fname = f"{start}_{end}{files[filenumber]}"
+ffmpeg_extract_subclip(files[filenumber], start, end, targetname=fname)
+
+print(fname)
+# print(YEAR, MONTH, DATE, HOUR, MINUTE, SECONDS)
